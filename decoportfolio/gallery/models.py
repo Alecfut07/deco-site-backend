7bab29e27144cc6256dfbcef6b7ba8013ae43940
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-08-13 15:25:19 -0700

    Create a PorfolioItem model

--- decoportfolio/gallery/models.py
+++ decoportfolio/gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class PortfolioItem(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='porfolio/')
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=100, blank=True)
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-upload_date']
+
+    def __str__(self):
+        return self.title
