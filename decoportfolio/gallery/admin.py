b2e047572ba3f99352f4e0368538591b1fc0193b
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-08-13 15:39:23 -0700

    Register PortfolioItem model in Admin

--- decoportfolio/gallery/admin.py
+++ decoportfolio/gallery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PortfolioItem
 
-# Register your models here.
+@admin.register(PortfolioItem)
+class PortfolioItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'upload_date']
+    list_filter = ['category', 'upload_date']
+    search_fields = ['title', 'description']
+    ordering = ['-upload_date']
+    readonly_fields = ['upload_date']
