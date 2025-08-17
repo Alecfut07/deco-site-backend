74bf4745b8702128fc8f2f080adf5c5bf890c264
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-08-17 15:52:55 -0700

    Create the category model

--- decoportfolio/category/models.py
+++ decoportfolio/category/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    display_order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['display_order', 'name']
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
