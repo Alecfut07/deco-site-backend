d0ca14e08abc54a54c4d297ebc7e9460c66f92da
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-08-18 15:56:42 -0700

    Add a service field to link portfolio items to specific services

--- decoportfolio/gallery/models.py
+++ decoportfolio/gallery/models.py
@@ -2,19 +2,6 @@ from django.db import models
 from django.utils import timezone
 from category.models import Category
 
-class PortfolioItem(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='porfolio/')
-    description = models.TextField(blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='portfolio_items')
-    upload_date = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-upload_date']
-
-    def __str__(self):
-        return self.title
-    
 class Service(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
@@ -29,3 +16,17 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+    
+class PortfolioItem(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='porfolio/')
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='portfolio_items')
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True, blank=True, related_name='portfolio_items')
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-upload_date']
+
+    def __str__(self):
+        return self.title
