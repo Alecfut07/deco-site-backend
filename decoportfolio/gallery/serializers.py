85227d6e7ecfa5c177617e408537013bea169320
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-08-25 17:09:40 -0700

    Create serializers of Category, Service and PortfolioItem

---
+++ decoportfolio/gallery/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import PortfolioItem, Category, Service
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'display_order']
+
+class ServiceSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Service
+        fields = ['id', 'name', 'category', 'price_range', 'category', 'is_active', 'display_order']
+
+class PortfolioItemSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    service = ServiceSerializer(read_only=True)
+    image_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = PortfolioItem
+        fields = ['id', 'title', 'description', 'category', 'service', 'image_url', 'upload_date']
+
+    def get_image_url(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.image.url)
+            return obj.image.url
+        return None
