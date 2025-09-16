5957a23abd217735cb13b2c3f513f043a785f566
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-09-15 17:59:53 -0700

    Create new model of BusinessInfo

--- decoportfolio/gallery/models.py
+++ decoportfolio/gallery/models.py
@@ -2,6 +2,26 @@ from django.db import models
 from django.utils import timezone
 from category.models import Category
 
+class BusinessInfo(models.Model):
+    company_name = models.CharField(max_length=200)
+    tagline = models.CharField(max_length=300, blank=True)
+    description = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=20)
+    address = models.TextField(blank=True)
+    years_experience = models.IntegerField(default=0)
+    specialties = models.TextField(help_text="Comma-separated list of specialties")
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Business Information'
+        verbose_name_plural = 'Business Information'
+
+    def __str__(self):
+        return self.company_name
+
 class Service(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
