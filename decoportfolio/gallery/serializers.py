93a9f22d4a67df79f75ed9913f22b77f94c83bbf
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-09-16 18:28:26 -0700

    Update the portfolio item serializer with the new fields: images, before/after image

--- decoportfolio/gallery/serializers.py
+++ decoportfolio/gallery/serializers.py
@@ -24,16 +24,48 @@ class ServiceSerializer(serializers.ModelSerializer):
 class PortfolioItemSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     service = ServiceSerializer(read_only=True)
-    image_url = serializers.SerializerMethodField()
+    primary_image_url = serializers.SerializerMethodField()
+    before_image_url = serializers.SerializerMethodField()
+    after_image_url = serializers.SerializerMethodField()
+    image_count = serializers.SerializerMethodField()
+    has_before_after = serializers.SerializerMethodField()
 
     class Meta:
         model = PortfolioItem
-        fields = ['id', 'title', 'description', 'category', 'service', 'image_url', 'upload_date']
+        fields = [
+            'id', 'title', 'description', 'category', 'service',
+            'images', 'primary_image_url', 'image_count',
+            'before_image_url', 'after_image_url', 'has_before_after',
+            'is_before_after', 'upload_date'
+        ]
 
-    def get_image_url(self, obj):
-        if obj.image:
+    def get_primary_image_url(self, obj):
+        primary_image = obj.get_primary_image()
+        if primary_image:
             request = self.context.get('request')
             if request:
-                return request.build_absolute_uri(obj.image.url)
-            return obj.image.url
+                return request.build_absolute_uri(primary_image)
+            return primary_image
         return None
+    
+    def get_before_image_url(self, obj):
+        if obj.before_image:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.before_image.url)
+            return obj.before_image.url
+        return None
+    
+    def get_after_image_url(self, obj):
+        if obj.after_image:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.after_image.url)
+            return obj.after_image.url
+        return None
+        
+    def get_image_count(self, obj):
+        return len(obj.images) if obj.images else 0
+
+    def get_has_before_after(self, obj):
+        return obj.has_before_after()
