72b02bad7b626810c8c20ca602ef6c4fd25a5a7e
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-09-29 18:11:43 -0700

    Create new class: AdvancedCache to caching with versioning and compression data, and add method of _compress_data if the data is large enough

---
+++ decoportfolio/gallery/utils.py
@@ -0,0 +1,23 @@
+import json
+import gzip
+import base64
+import hashlib
+from django.core.cache import cache
+from django.conf import settings
+
+class AdvancedCache:
+    """Advanced caching with versioning and compression"""
+
+    CACHE_VERSION = 1
+    COMPRESSION_THRESHOLD = 1024 # Compress data larger than 1KB
+
+    @classmethod
+    def _compress_data(cls, data):
+        """Compress data if it's large enough"""
+        json_data = json.dumps(data)
+        if len(json_data) > cls.COMPRESSION_THRESHOLD:
+            compressed = gzip.compress(json_data.encode('utf-8'))
+            return base64.b64encode(compressed).decode('utf-8'), True
+        return json_data, False
+    
+    
