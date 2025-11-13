82ea78e3fad66ccf0e90c6bc255186af3ab635c1
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-11-12 20:26:33 -0800

    Create a Family group automatically

--- decoportfolio/gallery/apps.py
+++ decoportfolio/gallery/apps.py
@@ -6,4 +6,8 @@ class GalleryConfig(AppConfig):
     name = 'gallery'
 
     def ready(self):
+        from django.db.models.signals import post_migrate
+        from .signals import ensure_family_group_exists
         import gallery.signals # This ensures signals are loaded
+
+        post_migrate.connect(ensure_family_group_exists, sender=self)
