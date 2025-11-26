10f4cb5ba0342973f70f812fb27ed8bbb25b5ff9
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-11-26 15:48:36 -0800

    Change TokenAuthentication to FamilyMemberTokenAuthentication for all AdminViewSets

--- decoportfolio/gallery/views/admin.py
+++ decoportfolio/gallery/views/admin.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from gallery.models import (
     PortfolioItem,
     PortfolioImage,
@@ -17,45 +18,46 @@ from gallery.serializers import (
     BusinessInfoSerializer,
 )
 from gallery.permissions import IsFamilyMember
+from gallery.views.auth import FamilyMemberTokenAuthentication
 
 
 class PortfolioItemAdminViewSet(ModelViewSet):
     queryset = PortfolioItem.objects.all()
     serializer_class = PortfolioItemSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
 
 
 class PortfolioImageAdminViewSet(ModelViewSet):
     queryset = PortfolioImage.objects.select_related("portfolio_item")
     serializer_class = PortfolioImageSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
 
 
 class PortfolioVideoAdminViewSet(ModelViewSet):
     queryset = PortfolioVideo.objects.select_related("portfolio_item")
     serializer_class = PortfolioVideoSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
 
 
 class CategoryAdminViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
 
 
 class ServiceAdminViewSet(ModelViewSet):
     queryset = Service.objects.select_related("category")
     serializer_class = ServiceSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
 
 
 class BusinessInfoAdminViewSet(ModelViewSet):
     queryset = BusinessInfo.objects.all()
     serializer_class = BusinessInfoSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
