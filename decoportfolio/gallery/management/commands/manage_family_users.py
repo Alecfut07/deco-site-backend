cfbba6dba3725874f5ae0f608fcc6891eecd4b88
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-11-26 16:54:28 -0800

    Manage FamilyMember users (separate from Django Admin users), show list all FamilyMember user, create a new FamilyMember user, Add existing FamilyMember user, Deactivate a FamilyMember user and acttivate a FamilyMember user

--- decoportfolio/gallery/management/commands/manage_family_users.py
+++ decoportfolio/gallery/management/commands/manage_family_users.py
@@ -1,94 +1,125 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User, Group
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from gallery.models import FamilyMember
 
 
 class Command(BaseCommand):
-    help = "Manage users in the Family group"
+    help = "Manage FamilyMember users (separate from Django Admin users)"
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--add",
-            type=str,
-            help="Add a user to the Family group (provide username)",
-        )
-        parser.add_argument(
-            "--remove",
-            type=str,
-            help="Remove a user from the Family group (provide username)",
-        )
-        parser.add_argument(
             "--list",
             action="store_true",
-            help="List all users in the Family group",
+            help="List all FamilyMember users",
         )
         parser.add_argument(
             "--create-user",
             type=str,
             nargs=2,
             metavar=("USERNAME", "EMAIL"),
-            help="Create a new user and add to Family group (username, email)",
+            help="Create a new FamilyMember user",
+        )
+        parser.add_argument(
+            "--add",
+            type=str,
+            metavar="USERNAME",
+            help="Add existing FamilyMember user (no-op, users are automatically family members)",
+        )
+        parser.add_argument(
+            "--remove",
+            type=str,
+            metavar="USERNAME",
+            help="Deactivate a FamilyMember user",
+        )
+        parser.add_argument(
+            "--activate",
+            type=str,
+            metavar="USERNAME",
+            help="Activate a FamilyMember user",
         )
 
     def handle(self, *args, **options):
-        # Get or create Family group
-        family_group, created = Group.objects.get_or_create(name="Family")
-        if created:
-            self.stdout.write(self.style.SUCCESS(f"Successfully created Family group"))
-        else:
-            self.stdout.write(f"Family group already exists")
-        # Add user to Family group
-        if options["add"]:
-            username = options["add"]
-            try:
-                user = User.objects.get(username=username)
-                family_group.user_set.add(user)
-                self.stdout.write(
-                    self.style.SUCCESS(f"Successfully added {username} to Family group")
-                )
-            except User.DoesNotExist:
-                self.stdout.write(self.style.ERROR(f"User {username} does not exist"))
-        # Remove user from Family group
-        if options["remove"]:
-            username = options["remove"]
-            try:
-                user = User.objects.get(username=username)
-                family_group.user_set.remove(user)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Successfully removed {username} from Family group"
-                    )
-                )
-            except User.DoesNotExist:
-                self.stdout.write(self.style.ERROR(f"User {username} does not exist"))
-        # List all Family group users
         if options["list"]:
-            family_members = family_group.user_set.all()
-            if family_members:
-                self.stdout.write(self.style.SUCCESS("\nFamily Group Members:"))
-                for user in family_members:
-                    status = " (superuser)" if user.is_superuser else ""
-                    self.stdout.write(f"  - {user.username} ({user.email}){status}")
-            else:
-                self.stdout.write(self.style.WARNING("No users in Family group"))
-        # Create new user and add to Family group
-        if options["create_user"]:
+            self.list_users()
+        elif options["create_user"]:
             username, email = options["create_user"]
-            if User.objects.filter(username=username).exists():
-                self.stdout.write(self.style.ERROR(f"User {username} already exists"))
-            else:
-                # Create user (password will need to be set separately)
-                user = User.objects.create_user(
-                    username=username,
-                    email=email,
-                    password="temp_password_change_me",  # User should change this
+            self.create_user(username, email)
+        elif options["add"]:
+            self.stdout.write(
+                self.style.WARNING(
+                    "All FamilyMember users are automatically family members. "
+                    "Use --activate to activate a user."
                 )
-                family_group.user_set.add(user)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Successfully created user {username} and added to Family group.\n"
-                        f"Please set password with: python manage.py changepassword {username}"
-                    )
+            )
+        elif options["remove"]:
+            self.deactivate_user(options["remove"])
+        elif options["activate"]:
+            self.activate_user(options["activate"])
+        else:
+            self.stdout.write(
+                self.style.ERROR("Please specify an action. Use --help for options.")
+            )
+
+    def list_users(self):
+        users = FamilyMember.objects.all().order_by("username")
+        if users.exists():
+            self.stdout.write(
+                self.style.SUCCESS(f"\nFound {users.count()} FamilyMember user(s):\n")
+            )
+            for user in users:
+                status = "Active" if user.is_active else "Inactive"
+                self.stdout.write(f"  - {user.username} ({user.email}) - {status}")
+        else:
+            self.stdout.write(self.style.WARNING("No FamilyMember users found."))
+
+    def create_user(self, username, email):
+        if FamilyMember.objects.filter(username=username).exists():
+            self.stdout.write(
+                self.style.WARNING(f"FamilyMember user {username} already exists.")
+            )
+            return
+
+        if FamilyMember.objects.filter(email=email).exists():
+            self.stdout.write(self.style.WARNING(f"Email {email} is already in use"))
+            return
+
+        user = FamilyMember.objects.create_user(
+            username=username,
+            email=email,
+            password="temp_password_change_me",  # User should change this
+        )
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully created FamilyMember user {username}.\n"
+                f"Please set password with: python manage.py changepassword {username}"
+            )
+        )
+
+    def deactivate_user(self, username):
+        try:
+            user = FamilyMember.objects.get(username=username)
+            user.is_active = False
+            user.save()
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully deactivated FamilyMember user {username}"
                 )
+            )
+        except FamilyMember.DoesNotExist:
+            self.stdout.write(
+                self.style.ERROR(f"FamilyMember user {username} does not exist")
+            )
+
+    def activate_user(self, username):
+        try:
+            user = FamilyMember.objects.get(username=username)
+            user.is_active = True
+            user.save()
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully activated FamilyMember user {username}"
+                )
+            )
+        except FamilyMember.DoesNotExist:
+            self.stdout.write(
+                self.style.ERROR(f"FamilyMember user {username} does not exist")
+            )
