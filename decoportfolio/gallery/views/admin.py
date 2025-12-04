cd2dcbf730e45e4e6cafecb274c5002282d34fdf
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2025-12-03 22:04:06 -0800

    Change response status http from 204 to 200

--- decoportfolio/gallery/views/admin.py
+++ decoportfolio/gallery/views/admin.py
@@ -1,6 +1,8 @@
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from gallery.models import (
     PortfolioItem,
     PortfolioImage,
@@ -27,12 +29,29 @@ class PortfolioItemAdminViewSet(ModelViewSet):
     authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        item_title = instance.title
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": f"Portfolio item {item_title} deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
+
 
 class PortfolioImageAdminViewSet(ModelViewSet):
     queryset = PortfolioImage.objects.select_related("portfolio_item")
     serializer_class = PortfolioImageSerializer
     authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": "Portfolio image deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
 
 
 class PortfolioVideoAdminViewSet(ModelViewSet):
@@ -41,12 +60,29 @@ class PortfolioVideoAdminViewSet(ModelViewSet):
     authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": "Portfolio video deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
+
 
 class CategoryAdminViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        category_name = instance.name
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": f"Category {category_name} deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
 
 
 class ServiceAdminViewSet(ModelViewSet):
@@ -55,9 +91,26 @@ class ServiceAdminViewSet(ModelViewSet):
     authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        service_name = instance.name
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": f"Service {service_name} deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
+
 
 class BusinessInfoAdminViewSet(ModelViewSet):
     queryset = BusinessInfo.objects.all()
     serializer_class = BusinessInfoSerializer
     authentication_classes = [FamilyMemberTokenAuthentication]
     permission_classes = [IsFamilyMember]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": "Business info deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
