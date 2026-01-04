5e221c81011fea4a28692e9173d05e2d90690780
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2026-01-03 18:08:33 -0800

    Add the missing method to handle the PATCH (partial update) for the Business Info AdminViewSet

--- decoportfolio/gallery/views/admin.py
+++ decoportfolio/gallery/views/admin.py
@@ -148,6 +148,17 @@ class BusinessInfoAdminViewSet(ModelViewSet):
         self.perform_update(serializer)
         return Response(serializer.data)
 
+    def partial_update(self, request, *args, **kwargs):
+        """
+        Handle PATCH request to partially update BusinessInfo.
+        Works with or without ID in URL.
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
