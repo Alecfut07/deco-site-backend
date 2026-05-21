d0cb84c0d73f01b3c922d0afdd273441e9a320ab
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2026-05-21 16:41:03 -0700

    Move Django admin to another path

--- decoportfolio/decoportfolio/urls.py
+++ decoportfolio/decoportfolio/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('gallery.urls')),
+    path("django-admin/", admin.site.urls),
+    path("", include("gallery.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
